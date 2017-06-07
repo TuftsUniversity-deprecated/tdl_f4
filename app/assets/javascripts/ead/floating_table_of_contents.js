fe94a934723b23df891b94cf8b5a1d9f2006e1b9
bgoodmon <brian.goodmon@tufts.edu>
2017-06-07 11:52:03 -0400

    Minor changes to some ead code - whitespace changes, removing debugging output.

--- app/assets/javascripts/ead/floating_table_of_contents.js
+++ app/assets/javascripts/ead/floating_table_of_contents.js
@@ -2,7 +2,6 @@ function handleTOC(eventObj) {
   var outermost = $('#outermost')
   var column = $('#floating_table_of_contents_column')
   var toc = $('#floating_table_of_contents')
-  var window_left = $(window).scrollLeft();
   var window_top = $(window).scrollTop();
   var outermost_height = outermost.height();
   var toc_top = window_top - column.position().top;
@@ -12,18 +11,9 @@ function handleTOC(eventObj) {
   var toc_at_bottom = (toc_space < toc_height);
 
   if ((eventObj.type == "resize") || toc_at_top || toc_at_bottom) {
-console.log("==========================================");
-/*console.log("window_top is " + window_top);
-console.log("outermost_height is " + outermost_height);
-console.log("column.position().top is " + column.position().top);
-console.log(">>> column.offset().top is " + column.offset().top);
-console.log("toc_top is " + toc_top);
-console.log("toc_space is " + toc_space);
-console.log("toc_height is " + toc_height);*/
     // browser is resizing, or page is scrolled up or down to the point at which TOC should stick
     // to the top or bottom 
     toc.css('position', 'relative');
-console.log("sticking left at 0px");
     toc.css('left', '0px');
 
     if (toc_at_top) {
@@ -38,20 +28,9 @@ console.log("sticking left at 0px");
     }
   } else {
     // TOC should float
-console.log("------------------------------------------");
-/*console.log("window_top is " + window_top);
-console.log("outermost_height is " + outermost_height);
-console.log("column.position().top is " + column.position().top);
-console.log(">>> column.offset().top is " + column.offset().top);
-console.log("toc_top is " + toc_top);
-console.log("toc_space is " + toc_space);
-console.log("toc_height is " + toc_height);*/
     toc.css('position', 'fixed');
-    toc.css('left', column.offset().left - window_left);
-console.log("floating -  column.offset().left is " + column.offset().left);
-console.log("floating -  window_left is " + window_left);
-console.log("floating -  column.offset().left - window_left is " + (column.offset().left - window_left));
-    toc.css('top', '0px');
+    toc.css('left', column.offset().left);
+    toc.css('top', '132px');
   }
 }
 
